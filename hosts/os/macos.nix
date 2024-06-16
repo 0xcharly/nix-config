9e64649461639781b8dca14afcfcdca8b7eb81dc
Charly Delay <charly@delay.gg>
2024-06-17 00:02:14 +0900

    Enable touchID sudo

--- hosts/os/macos.nix
+++ hosts/os/macos.nix
@@ -60,6 +60,9 @@
     ];
   };
 
+  # Enable the `sudo` touch ID prompt.
+  security.pam.enableSudoTouchIdAuth = true;
+
   # The user should already exist, but we need to set this up so Nix knows
   # what our home directory is (https://github.com/LnL7/nix-darwin/issues/423).
   users.users.delay = {
