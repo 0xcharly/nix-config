de0f0b69fbc1754e1fb3531dd5eb45e767e80dec
Charly Delay <charly@delay.gg>
2024-06-17 21:51:34 +0900

    Only install homebrew on darwin

--- lib/mksystem.nix
+++ lib/mksystem.nix
@@ -25,35 +25,28 @@ in
   osSystem {
     specialArgs = {inherit isCorpManaged isHeadless;};
 
-    modules = [
-      # Apply our overlays.
-      {nixpkgs.overlays = overlays;}
+    modules =
+      [
+        # Apply our overlays.
+        {nixpkgs.overlays = overlays;}
 
-      configuration
-      hmModules.home-manager
-      {
-        home-manager.extraSpecialArgs = {inherit inputs isCorpManaged isHeadless;};
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.backupFileExtension = "nix-backup";
-        home-manager.users.${user} = import ../users/${user}/home-manager.nix;
-      }
-
-      homebrew.darwinModules.nix-homebrew
-      {
-        nix-homebrew = {
-          # Install Homebrew under the default prefix
-          enable = true;
-
-          # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-          enableRosetta = false;
-
-          # User owning the Homebrew prefix
-          inherit user;
-
-          # Automatically migrate existing Homebrew installations
-          # autoMigrate = true;
-        };
-      }
-    ];
+        configuration
+        hmModules.home-manager
+        {
+          home-manager.extraSpecialArgs = {inherit inputs isCorpManaged isHeadless;};
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "nix-backup";
+          home-manager.users.${user} = import ../users/${user}/home-manager.nix;
+        }
+      ]
+      ++ nixpkgs.lib.optionals isDarwin [
+        homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true; # Install Homebrew under the default prefix
+            inherit user; # User owning the Homebrew prefix
+          };
+        }
+      ];
   }
