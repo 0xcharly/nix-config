0a14392ab5ff53eaf92e286fe2205153715b3231
Charly Delay <charly@delay.gg>
2024-06-23 21:26:59 +0900

    Unbork the macOS dock

--- hosts/os/macos.nix
+++ hosts/os/macos.nix
@@ -64,6 +64,31 @@
   # Enable the `sudo` touch ID prompt.
   security.pam.enableSudoTouchIdAuth = true;
 
+  # Unbork the dock.
+  system.defaults.dock = {
+    autohide = true;
+    autohide-delay = 0.0;
+    autohide-time-modifier = 0.4;
+    orientation = "left";
+    persistent-apps = [];
+    persistent-others = [];
+    show-process-indicators = false;
+    show-recents = false;
+    static-only = true;
+
+    tilesize = 32;
+    magnification = true;
+    largesize = 48;
+
+    # Disable hot corners.
+    wvous-tl-corner = 1;
+    wvous-bl-corner = 1;
+    wvous-tr-corner = 1;
+    wvous-br-corner = 1;
+
+    mru-spaces = false;
+  };
+
   # The user should already exist, but we need to set this up so Nix knows
   # what our home directory is (https://github.com/LnL7/nix-darwin/issues/423).
   users.users.delay = {
