5a700b67bbcd3b3207304f1b2a11ca338e223a38
Charly Delay <charly@delay.gg>
2024-06-27 23:29:33 +0900

    Remove obsolete nixvim config

--- users/delay/nvim/auto-hlsearch.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ...}:
-{
-  plugin = pkgs.vimPlugins.auto-hlsearch-nvim;
-  config = ''
-    lua require 'auto-hlsearch'.setup()
-  '';
-}
--- users/delay/nvim/common.nix
+++
@@ -1,9 +0,0 @@
-{
-  # { error = '󰅗 󰅙 󰅘 󰅚 󱄊 ', warn = '󰀨 󰗖 󱇎 󱇏 󰲼 ', info = '󰋽 󱔢 ', hint = '󰲽 ' },
-  diagnostic_signs = {
-    error = "󰅚 ";
-    warn = "󰗖 ";
-    info = "󰋽 ";
-    hint = "󰲽 ";
-  };
-}
