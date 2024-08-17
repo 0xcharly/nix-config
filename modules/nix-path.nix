a995ccb8206034328fb663684c0685c72462fa7d
Charly Delay <charly@delay.gg>
2024-08-17 23:51:32 +0900

    Set this flake's nixpkgs as <nixpkgs> when running `nix run nixpkgs#…`

--- modules/nix-path.nix
+++ modules/nix-path.nix
@@ -11,5 +11,11 @@ in {
   nix.nixPath = [
     "nixpkgs=${nixpkgs}"
     "nixpkgs-unstable=${inputs.nixpkgs-unstable}"
+    "home-manager=${inputs.home-manager}"
   ];
+
+  nix.registry = {
+    # Makes `nix run nixpkgs#…` run using the nixpkgs from this flake
+    nixpkgs.flake = nixpkgs;
+  };
 }
