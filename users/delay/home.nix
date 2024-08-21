297a490a53789f1f4980480a0e06f30a13a1489d
Charly Delay <charly@delay.gg>
2024-08-21 23:19:55 +0900

    Remove duplicated config

--- users/delay/home.nix
+++ users/delay/home.nix
@@ -34,21 +34,6 @@ in {
 
   home.stateVersion = "24.05";
 
-  # Allow HM to manage itself when in standalone mode.
-  # This makes the home-manager command available to users.
-  programs.home-manager.enable = true;
-
-  # Try to save some space by not installing variants of the
-  # home-manager manual. Unlike what the name implies, this
-  # section is for home-manager related manpages only, and does
-  # not affect whether or not manpages of actual packages will
-  # be installed.
-  manual = {
-    manpages.enable = false;
-    html.enable = false;
-    json.enable = false;
-  };
-
   #---------------------------------------------------------------------
   # Packages
   #---------------------------------------------------------------------
