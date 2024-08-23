dfbab3a6315a1b404f80775df170586d3200d812
Charly Delay <charly@delay.gg>
2024-08-23 23:19:50 +0900

    Rework fish's prompt

--- users/delay/config.fish
+++ users/delay/config.fish
@@ -4,15 +4,14 @@ set -g fish_term24bit 1 # Enable true color support.
 fish_vi_key_bindings # Enable vi bindings.
 
 function fish_prompt
-  set_color --bold brgrey
-  string repeat --count $SHLVL --no-newline ":"
-  printf " "
+  set_color --bold blue
+  printf "_ "
   set_color normal
 end
 
 function transient_prompt_func
   set_color --bold brgrey
-  printf ": "
+  printf ">: "
   set_color normal
 end
 
@@ -40,10 +39,6 @@ function git_repo_get_name
 end
 
 function fish_right_prompt
-  set -l citc_space (citc_get_space_name)
-  set -l nix_shell (nix_shell_get_name)
-  set -l git_repo (git_repo_get_name)
-
   printf " "
   set_color black
   printf ""
@@ -52,29 +47,35 @@ function fish_right_prompt
   set_color normal; set_color black
   printf ""
   set_color normal
+
+  set -l citc_space (citc_get_space_name)
   if test -n "$citc_space"
     set_color magenta --reverse
     printf "   %s " $citc_space
-  else if test -n "$nix_shell"
-    set_color blue --reverse
-    printf " 󱄅  %s " $nix_shell
-  else if test -n "$git_repo"
-    set_color red --reverse
-    printf " 󰊢  %s " $git_repo
-  else
-    set_color green --reverse
-    printf " 󰉋  %s " (path basename $PWD)
-  end
-  set_color normal
-  if test -n "$citc_space"
+    set_color normal
     set_color magenta
-  else if test -n "$nix_shell"
-    set_color blue
-  else if test -n "$git_repo"
-    set_color red
   else
-    set_color green
+    set -l nix_shell (nix_shell_get_name)
+    if test -n "$nix_shell"
+      set_color blue --reverse
+      printf " 󱄅  %s " $nix_shell
+      set_color normal
+      set_color blue
+    else
+      set -l git_repo (git_repo_get_name)
+      if test -n "$git_repo"
+        set_color red --reverse
+        printf " 󰊢  %s " $git_repo
+        set_color normal
+        set_color red
+      else
+        set_color green --reverse
+        printf " 󰉋  %s " (path basename $PWD)
+        set_color normal
+        set_color green
+      end
+    end
   end
-  printf " "
+  printf ""
   set_color normal
 end
