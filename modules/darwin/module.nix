1f1319551e412a26d27b73c1b8321de332fdd4b5
Charly Delay <charly@delay.gg>
2024-08-23 20:29:28 +0900

    Remove redundant setting

--- modules/darwin/module.nix
+++ modules/darwin/module.nix
@@ -11,9 +11,6 @@
 
   modules.usrenv.compositor = "quartz";
 
-  # Mark admins as trusted users to enable cachix repositories.
-  nix.settings.trusted-users = ["@admin"];
-
   # We install Nix using a separate installer so we don't want nix-darwin
   # to manage it for us. This tells nix-darwin to just use whatever is running.
   nix.useDaemon = true;
