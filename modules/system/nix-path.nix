98bb772a65d04845744e9826a0e80eedf7ddbc59
Charly Delay <0@0xcharly.com>
2024-09-08 22:53:44 +0900

    Add missing `nixpkgs` in `NIX_PATH`

--- modules/system/nix-path.nix
+++ modules/system/nix-path.nix
@@ -1,5 +1,15 @@
-{inputs, ...}: {
-  nix.nixPath = [
+{
+  inputs,
+  pkgs,
+  ...
+}: {
+  nix.nixPath = let
+    nixpkgs =
+      if pkgs.stdenv.isDarwin
+      then inputs.nixpkgs-darwin
+      else inputs.nixpkgs;
+  in [
+    "nixpkgs=${nixpkgs}"
     "nixpkgs-unstable=${inputs.nixpkgs-unstable}"
     "home-manager=${inputs.home-manager}"
   ];
