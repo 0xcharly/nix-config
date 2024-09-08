83c39cbd89e953b50963c98848da37f7480e36fb
Charly Delay <0@0xcharly.com>
2024-09-08 21:53:12 +0900

    Fix TMUX theme

--- users/delay/home.nix
+++ users/delay/home.nix
@@ -171,7 +171,9 @@ in rec {
     mouse = true;
     secureSocket = true;
     sensibleOnTop = false;
-    catppuccin.enable = true;
+    # Do not force catppuccin theme here since it sets the "default" value to a
+    # solid color, which doesn't play well with translucent terminal background.
+    catppuccin.enable = false;
 
     extraConfig = builtins.readFile ./tmux.conf;
   };
