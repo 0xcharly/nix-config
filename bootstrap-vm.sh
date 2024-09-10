4ed444827a27f8d2d52299af5c26a5327a35a150
Charly Delay <0@0xcharly.com>
2024-09-11 00:35:56 +0900

    Reword notice at the end of `bootstrap-vm.sh`

--- bootstrap-vm.sh
+++ bootstrap-vm.sh
@@ -46,7 +46,7 @@ shutdown --reboot +1
 
 # Completion notice.
 echo -e "System installation \033[32;1mcomplete\033[0m. Rebooting in 1 minute."
-echo "After reboot, install Ghostty terminfo for best compatibility:"
+echo "After reboot, export your terminal's terminfo for best compatibility:"
 echo
 echo "  just ssh-copy-terminfo $SYSTEM_NAME"
 echo
