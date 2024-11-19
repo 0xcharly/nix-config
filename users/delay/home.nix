525c26447ecb01b9bfc1e7512375fb6e9c113c82
Charly Delay <0@0xcharly.com>
2024-11-20 00:08:30 +0900

    Do not install chromium on NixOS

--- users/delay/home.nix
+++ users/delay/home.nix
@@ -60,7 +60,6 @@ in rec {
     ++ lib.optionals hasWindowManager [pkgs.ghostty]
     ++ lib.optionals isLinux [pkgs.valgrind]
     ++ lib.optionals (isLinux && hasWindowManager) [
-      pkgs.chromium
       pkgs.firefox-devedition
       pkgs.rofi
       pkgs.ungoogled-chromium
