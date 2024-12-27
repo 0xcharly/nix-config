614bef07a704aebe9abaed415148448995e786b0
Charly Delay <0@0xcharly.com>
2024-12-28 01:35:20 +0900

    Update `deploy-linode.sh`

--- bin/deploy-linode.sh
+++ bin/deploy-linode.sh
@@ -42,11 +42,23 @@ nix run github:nix-community/nixos-anywhere -- \
 # Delete the temporary host keys created by the installation media.
 ssh-keygen -R "$REMOTE_ADDR" 2>/dev/null
 
-# Completion notice.
+# System install completion notice.
 echo -e "System installation \033[32;1mcomplete\033[0m. System rebooting."
 echo -e "\033[33;1mImportant\033[0m: Reboot the system with the correct configuration! "
+
+# Waiting for reboot and user input to continue.
+echo "Waiting for system reboot to deploy Home Manager for user \033[1m$USER\033[0m."
+read -n1 -rsp $'Press any key when the system is booted to continue or Ctrl+C to exit...\n'
+
+# Redeploy the system as user `$USER` to linn in Home Manager managed config.
+nixos-rebuild switch --flake .#linode --target-host "$USER@$REMOTE_ADDR"
+
+# Final completion notice.
+echo -e "Home Manager configuration \033[32;1mcomplete\033[0m."
 echo
-echo "After reboot, export your terminal's terminfo for best compatibility:"
+# TODO: remove the notice to copy terminfo in 25.05 when Ghostty's terminfo are
+# available in the stable channel.
+echo "Export your terminal's terminfo for best compatibility:"
 echo
 echo "  just ssh-copy-terminfo $REMOTE_ADDR"
 echo
