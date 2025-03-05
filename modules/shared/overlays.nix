378ee565d306cd45d734289dc97354bd698d0730
Charly Delay <delay@google.com>
2025-03-05 09:53:36 +0900

    Move copilot out of the default nvim plugins

--- modules/shared/overlays.nix
+++ modules/shared/overlays.nix
@@ -11,6 +11,12 @@
     inputs.nix-config-secrets.overlays.default
     inputs.rust-overlay.overlays.default
     inputs.zellij-prime-hopper.overlays.default
-    (final: prev: {nvim = prev.nix-config-nvim;})
+    (final: prev: {
+      # Inject Copilot's plugin late so it can be excluded from the corporate
+      # config.
+      nvim = prev.nix-config-nvim.override {
+        plugins = prev.plugins ++ [final.copilot-vim];
+      };
+    })
   ];
 }
