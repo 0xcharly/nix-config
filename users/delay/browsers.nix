1e2c744fa5d07bb4a52f5a6a045f81b3b360bd62
Charly Delay <0@0xcharly.com>
2025-03-09 15:11:09 +0900

    Fix NixOS Wiki search query

--- users/delay/browsers.nix
+++ users/delay/browsers.nix
@@ -191,7 +191,7 @@ in {
           };
 
           "NixOS Wiki" = {
-            urls = [{template = "https://wiki.nixos.org/index.php?search={searchTerms}";}];
+            urls = [{template = "https://wiki.nixos.org/w/index.php?search={searchTerms}";}];
             iconUpdateURL = "https://wiki.nixos.org/favicon.png";
             updateInterval = 24 * 60 * 60 * 1000; # Daily
             definedAliases = ["@nw"];
