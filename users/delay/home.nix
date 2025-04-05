d428528f4caafce10ce0f23973f29c813ad0b00c
Charly Delay <0@0xcharly.com>
2025-04-05 13:24:27 +0900

    Add `devenv` to installed packages

--- users/delay/home.nix
+++ users/delay/home.nix
@@ -31,21 +31,22 @@ in {
 
   # Packages I always want installed. Most packages I install using per-project
   # flakes sourced with direnv and nix-shell, so this is not a huge list.
-  home.packages =
+  home.packages = with pkgs;
     [
-      pkgs.coreutils # For consistency across platforms (i.e. GNU utils on macOS).
-      pkgs.duf # Modern `df` alternative.
-      pkgs.git-get # Used along with fzf and terminal multiplexers for repository management.
-      pkgs.libqalculate # Multi-purpose calculator on the command line.
-      pkgs.tree # List the content of directories in a tree-like format.
-      pkgs.yazi # File explorer that supports Kitty image protocol.
+      coreutils # For consistency across platforms (i.e. GNU utils on macOS).
+      devenv # For managing development environments.
+      duf # Modern `df` alternative.
+      git-get # Used along with fzf and terminal multiplexers for repository management.
+      libqalculate # Multi-purpose calculator on the command line.
+      tree # List the content of directories in a tree-like format.
+      yazi # File explorer that supports Kitty image protocol.
 
       # Our own package installed by overlay.
       # It's important to keep shadowing the original `pkgs.nvim` package
       # instead of referring to our custom config via another name to maintain
       # all related integrations (e.g. EDITOR) while being able to override it
       # at anytime (e.g. in the corp-specific flavor).
-      pkgs.nvim
+      nvim
     ]
     ++ lib.optionals isLinux [pkgs.valgrind];
 
