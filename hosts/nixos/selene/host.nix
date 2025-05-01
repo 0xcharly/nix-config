1d4bd475fe2205f0cecabe9f70bbc0a9047bc49d
Charly Delay <0@0xcharly.com>
2025-05-01 16:22:48 +0200

    Add Selene's config

---
+++ hosts/nixos/selene/host.nix
@@ -0,0 +1,62 @@
+{
+  pkgs,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  # See comment in modules/nixos/module.nix.
+  system.stateVersion = "24.11";
+
+  # Headless server.
+  modules.usrenv.compositor = "headless";
+
+  # Roles.
+  modules.system.roles.nixos = {
+    amdCpu = true;
+    noRgb = true;
+    tailscaleNode = true;
+    workstation = true;
+  };
+
+  modules.system.roles.nas = {
+    enable = true;
+    drives = {
+      nvme0 = "/dev/disk/by-id/nvme-Samsung_SSD_980_500GB_S78GNL0XC30952W"; # Front NVMe
+      nvme1 = "/dev/disk/by-id/nvme-Samsung_SSD_980_500GB_S78GNL0XC30991Y"; # Back NVMe
+      sata0 = "/dev/disk/by-id/ata-ST24000NT002-3N1101_ZYD2E1FW"; # SATA 1 / Blue
+      sata1 = "/dev/disk/by-id/ata-ST24000NT002-3N1101_ZYD20R4R"; # SATA 2 / Red
+      sata2 = "/dev/disk/by-id/ata-ST24000NT002-3N1101_ZYD254N6"; # SATA 3 / Yellow
+      sata3 = "/dev/disk/by-id/ata-ST24000NT002-3N1101_ZYD160A1"; # SATA 4 / Green
+    };
+  };
+
+  # Boot configuration.
+  boot.initrd.availableKernelModules = ["ahci" "xhci_pci" "nvme" "usbhid" "sd_mod"];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = [];
+  boot.extraModulePackages = [];
+
+  # IMPORTANT NOTE: Carefully check the latest kernel version that is compatible
+  # with the ZFS version in use.
+  # Compatible kernel versions are listed on the OpenZFS release page. Check
+  # which ZFS version is in use for the current stable channel.
+  # The current stable channel is 24.11, which uses ZFS 2.2.7, and is compatible
+  # with 4.18 - 6.12 kernels.
+  # https://discourse.nixos.org/t/zfs-latestcompatiblelinuxpackages-is-deprecated/52540
+  # https://github.com/openzfs/zfs/releases
+  boot.kernelPackages = pkgs.linuxPackages_6_12;
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.grub.enable = true;
+
+  # Network config.
+  networking.interfaces.enp15s0.useDHCP = true;
+
+  # The primary use case is to ensure when using ZFS that a pool isn’t imported
+  # accidentally on a wrong machine.
+  # https://search.nixos.org/options?channel=24.11&query=networking.hostId
+  networking.hostId = "af9964d6"; # head -c4 /dev/urandom | od -A none -t x4
+}
