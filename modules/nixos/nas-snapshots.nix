03449d5eebf3c665c7552c8e729d3705290ba975
Charly Delay <0@0xcharly.com>
2025-05-17 14:01:00 +0900

    Sanoid setup for regular ZFS datasets snapshots

--- modules/nixos/nas-snapshots.nix
+++ modules/nixos/nas-snapshots.nix
@@ -5,57 +5,74 @@
   ...
 }: let
   cfg = config.modules.system.roles.nas;
-  username = "backups";
-  group = "backups";
 in
   lib.mkIf cfg.enable {
-    users.users."${username}" = {
-      isSystemUser = true;
-      home = "/var/lib/${username}";
-      shell = "/usr/sbin/nologin";
-      createHome = true;
-      inherit group;
-      openssh.authorizedKeys.keys = lib.mkIf (!cfg.primary) [
-        ''command="${pkgs.sanoid}/bin/syncoid",restrict ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIf1BY82EBfuIPmqzPhA0SXNRQ9z7zdCzE99TiqdjWmg''
-      ];
-    };
-
-    users.groups."${group}" = {};
-
     environment.systemPackages = with pkgs; [
       mbuffer
       sanoid
     ];
 
+    # https://github.com/jimsalterjrs/sanoid/wiki/Sanoid#options.
+    services.sanoid = {
+      enable = true;
+
+      # Snapshot retention policy for backups/archives.
+      templates.archive = {
+        hourly = 0;
+        daily = 30;
+        monthly = 12;
+        yearly = 2;
+        autoprune = true;
+        autosnap = true;
+      };
+
+      # Snapshot retention policy for user files.
+      templates.users = {
+        hourly = 36;
+        daily = 30;
+        monthly = 3;
+        yearly = 0;
+        autoprune = true;
+        autosnap = true;
+      };
+
+      datasets = let
+        mkArchivePolicy = dataset: {
+          "${dataset}" = {
+            useTemplate = ["archive"];
+            recursive = true;
+            process_children_only = true;
+          };
+        };
+        mkUsersPolicy = dataset: {
+          "${dataset}" = {
+            useTemplate = ["users"];
+            recursive = true;
+            process_children_only = true;
+          };
+        };
+        mapPolicy = mkConfig: datasets:
+          lib.mergeAttrsList (builtins.map mkConfig datasets);
+      in
+        lib.mergeAttrsList [
+          # Backup every datasets under /tank/backups (but not the root dataset
+          # itself) with the `archive` policy.
+          (mapPolicy mkArchivePolicy [
+            "tank/backups"
+          ])
+          # Backup every datasets under /tank/ayako and /tank/delay (but not the
+          # root datasets themselves) with the `users` policy.
+          (mapPolicy mkUsersPolicy [
+            "tank/ayako"
+            "tank/delay"
+          ])
+        ];
+    };
+
+    # Only run sanoid when ZFS datasets are mounted and Tailscale network is up.
     systemd.services.sanoid = {
       after = ["network-online.target" "tailscaled.service" "zfs-mount-tank.service"];
       requires = ["network-online.target" "tailscaled.service" "zfs-mount-tank.service"];
       wants = ["tailscaled.service"];
-    };
-
-    systemd.services."${username}ZfsPermissions" = {
-      description = "Delegate ZFS permissions to the `${username}` user";
-      wantedBy = ["multi-user.target"];
-      after = ["zfs.target"];
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = let
-          permissions =
-            if cfg.primary
-            then "send,receive,snapshot,hold,release,mount"
-            else "receive,mount";
-          zfsAllow = pkgs.writeShellApplication {
-            name = "${username}-zfs-allow";
-            runtimeInputs = with pkgs; [zfs];
-            text = ''
-              for dataset in $(zfs list -H -o name -r tank); do
-                echo "Setting ZFS permissions for $dataset…"
-                zfs allow -u ${username} ${permissions} "$dataset"
-              done
-            '';
-          };
-        in
-          lib.getExe zfsAllow;
-      };
     };
   }
