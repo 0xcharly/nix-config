fd34d47b88cc9f604fb5b15eea43811b2bba56e8
Charly Delay <0@0xcharly.com>
2025-05-17 13:07:55 +0900

    Add Librewolf on NixOS

--- users/delay/browsers.nix
+++ users/delay/browsers.nix
@@ -8,6 +8,12 @@
     then args.osConfig
     else args.config;
   inherit (config.modules.usrenv) isCorpManaged isLinuxDesktop;
+
+  firefox-language-packs = ["en_US" "fr_FR" "ja_JP"];
+  firefox-profiles = import ./firefox/profiles.nix {
+    firefox-addons = pkgs.nur.repos.rycee.firefox-addons;
+    nixos-icons = pkgs.nixos-icons;
+  };
 in
   lib.mkIf isLinuxDesktop {
     # Only used when full-page translation is needed, or if the target website
@@ -19,7 +25,8 @@ in
           "--user-agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.2 Safari/605.1.15'"
         ];
       };
-    in lib.mkIf (!isCorpManaged) [google-chrome-for-jp-taxes];
+    in
+      lib.mkIf (!isCorpManaged) [google-chrome-for-jp-taxes];
 
     programs.chromium = {
       enable = !isCorpManaged;
@@ -36,10 +43,37 @@ in
       ];
     };
 
+    programs.librewolf = {
+      enable = true;
+      languagePacks = firefox-language-packs;
+
+      settings = {
+        # https://librewolf.net/docs/faq/#what-are-the-most-common-downsides-of-rfp-resist-fingerprinting
+        "privacy.resistFingerprinting" = false;
+        "privacy.fingerprintingProtection" = true;
+        "privacy.fingerprintingProtection.overrides" = "+AllTargets,-CSSPrefersColorScheme";
+      };
+
+      profiles = firefox-profiles;
+
+      # Check about:policies#documentation for options.
+      # Required for userChrome.
+      policies.Preferences."toolkit.legacyUserProfileCustomizations.stylesheets" = {
+        Value = true;
+        Status = "locked";
+      };
+    };
+
     programs.firefox = {
       enable = true;
       package = pkgs.firefox;
-      languagePacks = ["en_US" "fr_FR"];
+      languagePacks = firefox-language-packs;
+      profiles =
+        firefox-profiles
+        // {
+          # A somewhat more compact UI.
+          default.userChrome = builtins.readFile ./firefox/userChrome.css;
+        };
 
       # Check about:policies#documentation for options.
       policies = {
@@ -92,151 +126,6 @@ in
           "extensions.update.enabled" = lock false;
           "toolkit.legacyUserProfileCustomizations.stylesheets" = lock true; # Required for userChrome.
         };
-      };
-      profiles.default = {
-        id = 0;
-        isDefault = true;
-        name = "Default";
-        containers = {
-          primary = {
-            color = "blue";
-            icon = "fingerprint";
-            id = 1;
-          };
-          secondary = {
-            color = "yellow";
-            icon = "fingerprint";
-            id = 2;
-          };
-          spam = {
-            color = "orange";
-            icon = "fence";
-            id = 3;
-          };
-          dev = {
-            color = "toolbar";
-            icon = "fingerprint";
-            id = 4;
-          };
-          entertainment = {
-            color = "purple";
-            icon = "cart";
-            id = 5;
-          };
-          shopping = {
-            color = "pink";
-            icon = "cart";
-            id = 6;
-          };
-          banking = {
-            color = "green";
-            icon = "dollar";
-            id = 7;
-          };
-          dangerous = {
-            color = "red";
-            icon = "fingerprint";
-            id = 8;
-          };
-        };
-        containersForce = true;
-        # See: https://github.com/nix-community/nur-combined/blob/master/repos/rycee/pkgs/firefox-addons/generated-firefox-addons.nix
-        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-          consent-o-matic
-          cookie-autodelete
-          facebook-container
-          multi-account-containers
-          # NOTE: The 1Password database is regularly corrupted on restart.
-          # The assumption is that the add-on is auto-updated when firefox is
-          # running and then overwrote by subsequent `nixos-rebuild switch`.
-          # Add-ons auto-update was disabled for this reason. This may cause other
-          # issues in the future where the browser extension version gets out of
-          # sync with the desktop client.
-          # The alternative would be to manually install this extension.
-          onepassword-password-manager
-          privacy-badger
-          proton-pass
-          simple-translate
-          ublock-origin
-        ];
-
-        search = {
-          default = "Kagi Search";
-          # Workaround to Firefox replacing `search.json.mozlz4` symlink.
-          # https://github.com/nix-community/home-manager/issues/3698
-          force = true;
-          engines = {
-            "Kagi Search" = {
-              urls = [
-                {template = "https://kagi.com/search?q={searchTerms}";}
-                {
-                  template = "https://kagi.com/api/autosuggest?q={searchTerms}";
-                  type = "application/x-suggestions+json";
-                }
-              ];
-              iconUpdateURL = "https://github.com/kagisearch/browser_extensions/blob/e65c723370a2ee3960120612d2d46f3c9bfb6d87/shared/icons/icon_32px.png?raw=true";
-              updateInterval = 7 * 24 * 60 * 60 * 1000; # Weekly
-              definedAliases = ["k"];
-            };
-
-            "Nix Packages" = {
-              urls = [
-                {
-                  template = "https://search.nixos.org/packages";
-                  params = [
-                    {
-                      name = "type";
-                      value = "packages";
-                    }
-                    {
-                      name = "query";
-                      value = "{searchTerms}";
-                    }
-                  ];
-                }
-              ];
-
-              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = ["@np"];
-            };
-
-            "Home Manager Options" = {
-              urls = [{template = "https://home-manager-options.extranix.com/?query={searchTerms}";}];
-              iconUpdateURL = "https://home-manager-options.extranix.com/images/favicon.png";
-              updateInterval = 7 * 24 * 60 * 60 * 1000; # Weekly
-              definedAliases = ["@hm"];
-            };
-
-            "NixOS Wiki" = {
-              urls = [{template = "https://wiki.nixos.org/w/index.php?search={searchTerms}";}];
-              iconUpdateURL = "https://wiki.nixos.org/favicon.png";
-              updateInterval = 24 * 60 * 60 * 1000; # Daily
-              definedAliases = ["@nw"];
-            };
-
-            "GitHub Code Search" = {
-              urls = [{template = "https://github.com/search?q={searchTerms}&type=code";}];
-              iconUpdateURL = "https://github.com/fluidicon.png";
-              updateInterval = 7 * 24 * 60 * 60 * 1000; # Weekly
-              definedAliases = ["cs"];
-            };
-
-            # Hide the rest, we don't need it.
-            "Google".metaData.hidden = true;
-            "Amazon.com".metaData.hidden = true;
-            "Amazon.co.uk".metaData.hidden = true;
-            "Bing".metaData.hidden = true;
-            "eBay".metaData.hidden = true;
-            "DuckDuckGo".metaData.hidden = true;
-          };
-        };
-
-        settings = {
-          "extensions.autoDisableScopes" = 0;
-        };
-
-        # A somewhat more compact UI.
-        userChrome = builtins.readFile ./firefox/userChrome.css;
       };
     };
 
