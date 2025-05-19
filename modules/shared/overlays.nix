19363b7ad40c1296ca8327d8eec559aab13de0d0
Charly Delay <0@0xcharly.com>
2025-05-19 23:11:20 +0900

    Use custom nix-config-nvim overlay that automatically overrides `pkgs.nvim`

--- modules/shared/overlays.nix
+++ modules/shared/overlays.nix
@@ -8,14 +8,7 @@
     inputs.hyprpanel.overlay
     inputs.nur.overlays.default
     inputs.nix-config-fonts.overlays.default
-    inputs.nix-config-nvim.overlays.default
+    inputs.nix-config-nvim.overlays.nvim-override
     inputs.nix-config-secrets.overlays.default
-    (final: prev: {
-      # Inject Copilot's plugin late so it can be excluded from the corporate
-      # config.
-      nvim = prev.nix-config-nvim.override (old: {
-        plugins = old.plugins ++ [final.vimPlugins.copilot-vim];
-      });
-    })
   ];
 }
