30799bca556dcd598961a302668909cdf7c2453f
Charly Delay <delay@google.com>
2025-05-22 11:59:37 +0900

    Fixup

--- users/delay/raycast.nix
+++ users/delay/raycast.nix
@@ -5,7 +5,8 @@
 }: {
   # Raycast expects script attributes to be listed at the top of the file,
   # so a simple wrapper does not work. This *needs* to be a symlink.
-  xdg.configFile."raycast/bin/scrcpy" = lib.optionalAttrs pkgs.stdenv.isDarwin {
-    source = lib.getExe pkgs.raycast-scrcpy;
+  xdg = lib.optionalAttrs pkgs.stdenv.isDarwin {
+    enable = true;
+    configFile."raycast/bin/scrcpy".source = lib.getExe pkgs.raycast-scrcpy;
   };
 }
