3d40d0742b0a3b2bdd993cc79f8aa7e76e86d448
Charly Delay <0@0xcharly.com>
2025-06-01 22:13:05 +0900

    Update NixOS-on-Linode setup documentation

--- hosts/nixos/linode/host.nix
+++ hosts/nixos/linode/host.nix
@@ -1,12 +1,12 @@
 # (Derived from https://www.linode.com/docs/guides/install-nixos-on-linode/)
 #
-# 1. Create three (3) disk images:
+# 1. Create two disk images:
 #    - Installer: 2048mb (raw)
-#    - NixOS: rest (raw)
+#    - SYSTEM: rest (raw)
 #
 # 2. Boot in rescue mode with:
 #    - /dev/sda -> Installer
-#    - /dev/sdb -> NixOS
+#    - /dev/sdb -> SYSTEM
 #
 # 3. Once booted into Finnix (step 2):
 #      update-ca-certificates
@@ -15,14 +15,14 @@
 # 4. Create two configuration profiles:
 #    - Installer
 #      - Kernel: Direct Disk
-#      - /dev/sda -> NixOS
+#      - /dev/sda -> SYSTEM
 #      - /dev/sdb -> Installer
 #      - Root device: /dev/sdb
 #      - Helpers: distro and auto network helpers = off
 #      - Leave others on their defaults
 #    - Boot
 #      - Kernel: Direct Disk
-#      - /dev/sda -> NixOS
+#      - /dev/sda -> SYSTEM
 #      - Root device: /dev/sda
 #      - Helpers: distro and auto network helpers = off
 #      - Leave others on their defaults
@@ -30,12 +30,67 @@
 # 5. Boot into installer profile and setup session:
 #      passwd  # set nixos passwd
 #
-# 6. Get public IP address (ipconfig from the machine or from Linode UI).
+# 6. Get public IPv4 address (ipconfig from the machine or from Linode UI).
 #
 # 7. Remote install (from a x86_64-linux machine):
 #      just deploy-linode <ADDR>
 #
 # 8. Reboot into "Boot" profile.
+#
+# Optional: reclaim the installer partition
+#
+# 1. Shutdown the machine
+#
+# 2. With the Linode Manager console:
+#   a. Delete the "Installer" configuration
+#   a. Delete the "Installer" disk
+#   b. Resize the "SYSTEM" partition
+#
+# 3. Boot in rescue mode with:
+#    - /dev/sda -> SYSTEM
+#
+# 4. Use `parted` to resize the root partition.
+#   - It's possible that the swap partition, while being labeled #2, is actually
+#     physically _after_ the root partition
+#
+#     ```
+#     Device        Start      End  Sectors Size Type
+#     /dev/sda1      2048     4095     2048   1M BIOS boot
+#     /dev/sda2  46100480 48199679  2099200   1G Linux swap
+#     /dev/sda3      4096 46100479 46096384  22G Linux filesystem
+#
+#     Partition table entries are not in disk order.
+#     ```
+#
+#     If that's the case:
+#
+#     - Delete the swap partition:
+#       (parted) rm 2
+#
+#     - Resize the root partition:
+#       (parted) resizepart 3 -1GiB
+#
+#     - Recreate the swap partition:
+#       (parted) mkpart swap linux-swap -1GiB 100%
+#       (parted) name 2 swap
+#       (parted) set 2 swap on
+#
+# 5. Reboot into the system:
+#   a. Verify that BTRFS sees the slack:
+#       sudo btrfs filesystem usage /
+#
+#       ```
+#       Device size:                  21.98GiB
+#       Device slack:                  2.00GiB
+#       ```
+#
+#   b. Reclaim the slack:
+#       sudo btrfs filesystem resize max /
+#
+#       ```
+#       Device size:                  23.98GiB
+#       Device slack:                    0.00B
+#       ```
 {
   pkgs,
   modulesPath,
