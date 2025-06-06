393f6b948f528f5b83a955ffde11269bb986e8da
Charly Delay <0@0xcharly.com>
2025-06-07 00:02:32 +0900

    Update inputs

--- users/delay/wayland-hyprland.nix
+++ users/delay/wayland-hyprland.nix
@@ -153,13 +153,13 @@ in {
         # Chrome's Picture-in-Picture.
         "float, class:^$, title:^Picture in picture$"
         "pin, class:^$, title:^Picture in picture$"
-        "move 2043 35, class:^$, title:^Picture in picture$"
+        "move 2554 35, class:^$, title:^Picture in picture$"
         "size 512 288, class:^$, title:^Picture in picture$"
         "keepaspectratio, class:^$, title:^Picture in picture$"
         # Firefox's Picture-in-Picture.
         "float, class:^(firefox|librewolf)$, title:^Picture-in-Picture$"
         "pin, class:^(firefox|librewolf)$, title:^Picture-in-Picture$"
-        "move 2043 35, class:^(firefox|librewolf)$, title:^Picture-in-Picture$"
+        "move 2554 35, class:^(firefox|librewolf)$, title:^Picture-in-Picture$"
         "size 512 288, class:^(firefox|librewolf)$, title:^Picture-in-Picture$"
         "keepaspectratio, class:^(firefox|librewolf)$, title:^Picture-in-Picture$"
       ];
