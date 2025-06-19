42782d4fa39d73714a2618a06d81f00c8d5819f3
Charly Delay <delay@google.com>
2025-06-19 11:03:49 +0900

    Fix `tailscaleNode` boolean logic

--- modules/nixos/tailscale.nix
+++ modules/nixos/tailscale.nix
@@ -1,9 +1,10 @@
 {
   config,
   lib,
+  usrlib,
   ...
 }:
-lib.mkIf config.modules.system.networking.tailscaleNode {
+lib.mkIf (usrlib.bool.isTrue config.modules.system.networking.tailscaleNode) {
   services.tailscale = {
     enable = true;
     authKeyFile = config.age.secrets."services/tailscale-preauth.key".path;
