57ecf3feb888e1a4a560ebbec1a8340f2c7f4c18
Charly Delay <0@0xcharly.com>
2025-06-28 23:58:30 +0900

    Disable password login for `ayako`

--- modules/nixos/user-ayako.nix
+++ modules/nixos/user-ayako.nix
@@ -9,7 +9,6 @@ lib.mkIf config.modules.system.roles.nas.enable {
     isNormalUser = true;
     home = "/home/ayako";
     shell = pkgs.bash;
-    # Create a hashed password with `nix-shell -p mkpasswd --run "mkpasswd -m yescrypt"`
-    hashedPassword = "$y$j9T$TrN/LFDpdc5kkHZc6bkyV1$b4TqAfjBY2xuwmAUHVbco.cyI43JzOjs1dXt5ey3c.3";
+    hashedPassword = "*"; # Disable password login.
   };
 }
