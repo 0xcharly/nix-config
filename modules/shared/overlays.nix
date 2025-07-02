d2647d6a6e6f634ee263241270752eaacdec5ac6
Charly Delay <0@0xcharly.com>
2025-07-02 20:02:55 +0900

    Hotfix for the regression in the Linux kernel 6.15.4 which impacts Ghostty's performances

--- modules/shared/overlays.nix
+++ modules/shared/overlays.nix
@@ -12,5 +12,17 @@
     (_final: prev: {nvim = prev.nix-config-nvim.default;})
 
     inputs.nur.overlays.default
+
+    # Hotfix for Ghostty until a kernel fix.
+    # https://github.com/nixos/nixpkgs/issues/421442
+    (_final: prev: {
+      ghostty = prev.ghostty.overrideAttrs (_: {
+        preBuild = ''
+          shopt -s globstar
+          sed -i 's/^const xev = @import("xev");$/const xev = @import("xev").Epoll;/' **/*.zig
+          shopt -u globstar
+        '';
+      });
+    })
   ];
 }
