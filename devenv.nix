049473b3ca69daa52ccc11c8e92fec862e443851
Charly Delay <delay@google.com>
2025-07-02 12:34:34 +0900

    devenv: create `fmt` script to run `treefmt`

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,7 @@
 {
   inputs,
   pkgs,
+  lib,
   ...
 }: {
   packages = with pkgs; [
@@ -9,11 +10,21 @@
     just
 
     alejandra
-
-    # Formatters.
-    (inputs.treefmt-nix.lib.mkWrapper pkgs (import ./treefmt.nix))
   ];
 
   languages.nix.enable = true;
   languages.nix.lsp.package = pkgs.nixd;
+
+  scripts.fmt.exec = let
+    fmt-opts = {
+      projectRootFile = "flake.lock";
+      programs = {
+        alejandra.enable = true;
+        prettier.enable = true;
+        shfmt.enable = false;
+      };
+    };
+    fmt = inputs.treefmt-nix.lib.mkWrapper pkgs fmt-opts;
+  in
+    lib.getExe fmt;
 }
--- treefmt.nix
+++
@@ -1,8 +0,0 @@
-{
-  projectRootFile = "flake.lock";
-  programs = {
-    alejandra.enable = true;
-    prettier.enable = true;
-    shfmt.enable = false;
-  };
-}
