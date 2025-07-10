f945299c045c8563c929d6639a88dad11554faba
Charly Delay <0@0xcharly.com>
2025-07-10 19:25:38 +0900

    Update kernel and remove Ghostty's workaround

--- modules/shared/overlays.nix
+++ modules/shared/overlays.nix
@@ -7,22 +7,11 @@
     self.overlays.default
 
     inputs.nix-config-fonts.overlays.default
+
     # Override `pkgs.nvim` with custom distro.
     inputs.nix-config-nvim.overlays.default
     (_final: prev: {nvim = prev.nix-config-nvim.default;})
 
     inputs.nur.overlays.default
-
-    # Hotfix for Ghostty until a kernel fix.
-    # https://github.com/nixos/nixpkgs/issues/421442
-    (_final: prev: {
-      ghostty = prev.ghostty.overrideAttrs (_: {
-        preBuild = ''
-          shopt -s globstar
-          sed -i 's/^const xev = @import("xev");$/const xev = @import("xev").Epoll;/' **/*.zig
-          shopt -u globstar
-        '';
-      });
-    })
   ];
 }
