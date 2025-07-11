5c3a98499f74c6fa3264b20e964b1ade62d816ff
Charly Delay <0@0xcharly.com>
2025-07-11 22:29:53 +0900

    Comment cleanup

--- flake.nix
+++ flake.nix
@@ -12,16 +12,9 @@
     };
 
   inputs = {
-    # Pin our primary nixpkgs repositories. These are the main nixpkgs
-    # repositories we'll use for our configurations.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-    nur = {
-      url = "github:nix-community/NUR";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
     # Manages home directory, dotfiles and base environment.
     home-manager = {
@@ -29,33 +22,34 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # macOS system configuration with Nix.
-    nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
-      inputs.nixpkgs.follows = "nixpkgs-darwin";
-    };
-
-    # Filesystem management.
-    disko.url = "github:nix-community/disko";
-
-    # go/link service.
-    golink.url = "github:tailscale/golink";
-
-    # macOS only: Homebrew for Nix.
-    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
-
-    # Catppuccin all the things.
-    catppuccin.url = "github:catppuccin/nix";
-
-    # We use flake parts to organize our configurations.
+    # Convenience helpers for flake organization.
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
 
+    # Nix (community) Unofficial Repository.
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    disko.url = "github:nix-community/disko"; # Filesystem management.
+    golink.url = "github:tailscale/golink"; # go/link service.
+    catppuccin.url = "github:catppuccin/nix"; # Catppuccin all the things.
+
     nix-config-fonts.url = "github:0xcharly/nix-config-fonts"; # Unfree fonts.
     nix-config-nvim.url = "github:0xcharly/nix-config-nvim"; # Neovim.
     nix-config-secrets.url = "github:0xcharly/nix-config-secrets"; # Secrets management.
+
+    # macOS only: system configuration with Nix.
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+      inputs.nixpkgs.follows = "nixpkgs-darwin";
+    };
+
+    # macOS only: Homebrew for Nix.
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
   };
 
   nixConfig = {
