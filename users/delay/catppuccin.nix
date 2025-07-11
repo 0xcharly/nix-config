0aadb8bdf57239356db9ddf9ae2f7d26761bba01
Charly Delay <0@0xcharly.com>
2025-07-11 22:56:42 +0900

    Fix fzf catppuccin theme to use the Catppuccin Obsidian flavor

--- users/delay/catppuccin.nix
+++ users/delay/catppuccin.nix
@@ -1,15 +1,44 @@
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  ...
+}: {
   imports = [inputs.catppuccin.homeModules.catppuccin];
 
   # Configure catppuccin theme applied throughout the configuration.
   catppuccin = {
     flavor = "mocha";
+    accent = "blue";
 
     bat.enable = true;
     bottom.enable = true;
     fish.enable = true;
-    fzf.enable = true;
+    # TODO: figure out a better way to integrate with the github:catppuccin/nix config.
+    # fzf.enable = true;
     hyprland.enable = true;
     nushell.enable = true;
+  };
+
+  programs.fzf.colors = let
+    lavender = "#b4befe";
+    accent = lavender;
+    base = "#11181c";
+    text = "#e1e8f4";
+    rosewater = "#f5e0dc";
+    surface_blue = "#203147";
+    on_surface_blue = "#9fcdfe";
+  in {
+    bg = lib.mkForce base;
+    "bg+" = lib.mkForce surface_blue;
+    spinner = lib.mkForce rosewater;
+    hl = lib.mkForce on_surface_blue;
+    fg = lib.mkForce text;
+    header = lib.mkForce accent;
+    info = lib.mkForce accent;
+    pointer = lib.mkForce on_surface_blue;
+    marker = lib.mkForce accent;
+    prompt = lib.mkForce accent;
+    "fg+" = lib.mkForce text;
+    "hl+" = lib.mkForce on_surface_blue;
   };
 }
