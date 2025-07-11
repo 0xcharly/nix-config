461c354355ccd5e5dc76d05e52ef3e9aedcb0031
Charly Delay <0@0xcharly.com>
2025-07-11 22:24:56 +0900

    Update nix-config-nvim

--- modules/shared/overlays.nix
+++ modules/shared/overlays.nix
@@ -5,13 +5,8 @@
 }: {
   nixpkgs.overlays = [
     self.overlays.default
-
     inputs.nix-config-fonts.overlays.default
-
-    # Override `pkgs.nvim` with custom distro.
     inputs.nix-config-nvim.overlays.default
-    (_final: prev: {nvim = prev.nix-config-nvim.default;})
-
     inputs.nur.overlays.default
   ];
 }
