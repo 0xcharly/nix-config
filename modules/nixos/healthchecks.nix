39d70182c8893c6e7583cc76111e147a27af7587
Charly Delay <0@0xcharly.com>
2025-07-19 17:56:54 +0900

    Healthchecks: email setup

--- modules/nixos/healthchecks.nix
+++ modules/nixos/healthchecks.nix
@@ -12,8 +12,19 @@ in
         SITE_NAME = "Healthchecks";
         SITE_ROOT = "https://healthchecks.qyrnl.com";
 
-        REGISTRATION_OPEN = true;
+        REGISTRATION_OPEN = false;
         SECRET_KEY_FILE = config.age.secrets."services/healthchecks-secret-key".path;
+
+        # EMAIL_HOST = "localhost";
+        # EMAIL_PORT = "25";
+        # EMAIL_USE_TLS = "False";
+
+        EMAIL_HOST = "smtp.gmail.com";
+        EMAIL_PORT = "587";
+        EMAIL_HOST_USER = "jcd.delay@gmail.com";
+        EMAIL_HOST_PASSWORD_FILE = config.age.secrets."services/msmtp-gmail".path;
+
+        DEFAULT_FROM_EMAIL = "noreply@healthchecks.qyrnl.com";
       };
     };
 
