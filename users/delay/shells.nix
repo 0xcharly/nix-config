dc0279598801eeba9c2fe4e2c4c988c29e8142a9
Charly Delay <0@0xcharly.com>
2025-08-04 23:43:58 +0900

    Update nix-config-nvim

--- users/delay/shells.nix
+++ users/delay/shells.nix
@@ -6,9 +6,29 @@
   programs = {
     fish = {
       enable = true;
-      interactiveShellInit = builtins.readFile ./fish/config.fish;
+      interactiveShellInit = ''
+        set fish_greeting # Disable greeting message.
+        set -g fish_term24bit 1 # Enable true color support.
 
-      functions.fish_mode_prompt = ""; # Disable prompt vi mode reporting.
+        fish_vi_key_bindings # Enable vi bindings.
+
+        # Fixes cursor shape behavior in vim mode.
+        set fish_cursor_default block
+        set fish_cursor_insert block
+        set fish_cursor_replace_one block
+        set fish_cursor_replace block
+        set fish_cursor_external block
+        set fish_cursor_visual block
+      '';
+
+      functions = {
+        fish_mode_prompt = ""; # Disable prompt vi mode reporting.
+        fish_prompt = ''
+          set_color blue
+          printf "\$ "
+          set_color normal
+        '';
+      };
       shellAliases.nixsh = "nix-shell --run ${lib.getExe pkgs.fish}";
     };
 
