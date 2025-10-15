f927b24088ca36d4a95c9bd5bd11f980129c7dd8
Charly Delay <charly@delay.gg>
2025-10-15 23:11:37 +0900

    Fix bluetooth `powerOnBoot` setting

--- nix/modules/nixos/networking-bluetooth.nix
+++ nix/modules/nixos/networking-bluetooth.nix
@@ -15,27 +15,19 @@
     bluetooth = {
       enable = true;
       inherit (cfg) powerOnBoot;
-      settings = {
-        General =
-          {
-            # Shows battery charge of connected devices on supported
-            # Bluetooth adapters. Defaults to 'false'.
-            Experimental = true;
-            # When enabled other devices can connect faster to us, however
-            # the tradeoff is increased power consumption. Defaults to
-            # 'false'.
-            FastConnectable = cfg.enableFastConnectable;
-          }
-          // lib.optionalAttrs cfg.enableA2DPSink {
-            Enable = "Source,Sink,Media,Socket";
-          };
-        Policy = {
-          # Enable all controllers when they are found. This includes
-          # adapters present on start as well as adapters that are plugged
-          # in later on. Defaults to 'true'.
-          AutoEnable = true;
+      settings.General =
+        {
+          # Shows battery charge of connected devices on supported
+          # Bluetooth adapters. Defaults to 'false'.
+          Experimental = true;
+          # When enabled other devices can connect faster to us, however
+          # the tradeoff is increased power consumption. Defaults to
+          # 'false'.
+          FastConnectable = cfg.enableFastConnectable;
+        }
+        // lib.optionalAttrs cfg.enableA2DPSink {
+          Enable = "Source,Sink,Media,Socket";
         };
-      };
     };
   };
 }
