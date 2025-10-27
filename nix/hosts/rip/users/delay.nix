b3cd6f03a5c89a1398fd4f64d55dbf598f4d495e
Charly Delay <charly@delay.gg>
2025-10-27 23:05:56 +0900

    Add users modules for rip

---
+++ nix/hosts/rip/users/delay.nix
@@ -0,0 +1,16 @@
+{
+  flake,
+  inputs,
+  ...
+}: {
+  imports = [
+    inputs.nix-config-secrets.modules.home.blueprint
+    inputs.nix-config-secrets.modules.home.services-atuin
+
+    flake.modules.home.account-essentials
+    flake.modules.home.home-manager-nixos
+    flake.modules.home.secrets
+  ];
+
+  home.stateVersion = "25.05";
+}
