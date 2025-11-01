4631eb67506a4d8823b87cb0c4e0ad924042150e
Charly Delay <charly@delay.gg>
2025-11-01 17:16:31 +0900

    Fix udev rules to give access to the display

--- nix/modules/nixos/programs-asdcontrol.nix
+++ nix/modules/nixos/programs-asdcontrol.nix
@@ -2,8 +2,10 @@
   environment.defaultPackages = [flake.packages.${pkgs.system}.asdcontrol];
 
   # Studio Display (1114), Pro Display XDR (9243).
+  # KERNEL=="hiddev*", ATTRS{idVendor}=="05ac", ATTRS{idProduct}=="1114", GROUP="users", OWNER="root", MODE="0660"
+  # KERNEL=="hiddev*", ATTRS{idVendor}=="05ac", ATTRS{idProduct}=="9243", GROUP="users", OWNER="root", MODE="0660"
+  # SUBSYSTEM=="hidraw", DEVPATH=="*:1.7/????:05AC:1114.????/*", ATTRS{idVendor}=="05ac", ATTRS{idProduct}=="1114", MODE="0660", TAG+="uaccess"
   services.udev.extraRules = ''
-    KERNEL=="hiddev*", ATTRS{idVendor}=="05ac", ATTRS{idProduct}=="1114", GROUP="users", OWNER="root", MODE="0660"
-    KERNEL=="hiddev*", ATTRS{idVendor}=="05ac", ATTRS{idProduct}=="9243", GROUP="users", OWNER="root", MODE="0660"
+    SUBSYSTEM=="hidraw", DEVPATH=="*:1.?/????:05AC:9243.????/*", ATTRS{idVendor}=="05ac", ATTRS{idProduct}=="9243", MODE="0660", TAG+="uaccess"
   '';
 }
