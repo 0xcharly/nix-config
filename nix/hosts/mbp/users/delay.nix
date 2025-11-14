7608a0597ee25796b363ed17ffa0d268fc5d5d4a
Charly Delay <charly@delay.gg>
2025-11-14 22:03:51 +0900

    Remove SSH keys from darwin, it's badly broken because of age

--- nix/hosts/mbp/users/delay.nix
+++ nix/hosts/mbp/users/delay.nix
@@ -6,7 +6,6 @@
   imports = [
     inputs.nix-config-secrets.modules.home.blueprint
     inputs.nix-config-secrets.modules.home.services-atuin
-    inputs.nix-config-secrets.modules.home.ssh-keys-ring-3-tier
 
     flake.modules.home.account-essentials
     flake.modules.home.fonts
@@ -14,16 +13,12 @@
     flake.modules.home.home-manager-nixos
     flake.modules.home.jujutsu-deprecated
     flake.modules.home.keychain
-    flake.modules.home.secrets
-    flake.modules.home.ssh-keys
   ];
-
-  age.secretsDir = "/run/delay";
 
   home.stateVersion = "24.05";
 
   node = {
-    openssh.trusted-tier.ring = 3;
+    keychain.autoLoadTrustedKeys = false;
     # TODO: agenix HM module on darwin create paths that require shell evaluation…
     services.atuin.enableSync = false;
   };
