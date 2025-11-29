8561cd9a0ae65da6019454f46cccd083957318ad
Charly Delay <charly@delay.gg>
2025-11-29 16:05:43 +0900

    Fixed removed overlay (from older setup)

--- nix/modules/common/overlays.nix
+++ nix/modules/common/overlays.nix
@@ -1,10 +1,5 @@
-{
-  flake,
-  inputs,
-  ...
-}: {
+{inputs, ...}: {
   nixpkgs.overlays = [
-    flake.overlays.default
     (super: prev: {
       nvim = inputs.nix-config-nvim.packages.${super.stdenv.hostPlatform.system}.default;
     })
