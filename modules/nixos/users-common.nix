1af2cab84f4089be609e6740ea32229c5839ae91
Charly Delay <charly@delay.gg>
2025-12-12 23:00:44 +0900

    Minor comment fixup

--- modules/nixos/users-common.nix
+++ modules/nixos/users-common.nix
@@ -1,4 +1,4 @@
 {
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # TODO: fold into `access-directory.nix` once migration is completed.
   users.mutableUsers = false;
 }
