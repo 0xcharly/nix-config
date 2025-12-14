19c75ec374f151ada96be51c2cb938be1fd4fc91
Charly Delay <charly@delay.gg>
2025-12-14 22:27:51 +0900

    skl: prepare for new desktop machine role

--- hosts/skl/configuration.nix
+++ hosts/skl/configuration.nix
@@ -8,10 +8,11 @@
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
 
+    inputs.nix-config-colorscheme.modules.nixos.console
+
     inputs.nix-config-secrets.modules.nixos.blueprint
     inputs.nix-config-secrets.modules.nixos.nix-client-config
     inputs.nix-config-secrets.modules.nixos.services-tailscale
-    inputs.nix-config-secrets.modules.nixos.services-tailscale-initrd
     inputs.nix-config-secrets.modules.nixos.users-delay
 
     flake.modules.common.nix-client-config
@@ -23,41 +24,47 @@
     flake.modules.nixos.bootloader-systemd-boot
     flake.modules.nixos.fs-zfs-system
     flake.modules.nixos.fs-zfs-zpool-root
-    flake.modules.nixos.fs-zfs-zpool-root-data
     flake.modules.nixos.fs-zfs-zpool-root-home
     flake.modules.nixos.hardware-cpu-intel
     flake.modules.nixos.hardware-gpu-intel
-    flake.modules.nixos.initrd-unlock-over-ssh
-    flake.modules.nixos.initrd-tailscale
+    flake.modules.nixos.networking-bluetooth
     flake.modules.nixos.networking-wireless
     flake.modules.nixos.nix-client-config
     flake.modules.nixos.overlays
+    flake.modules.nixos.programs-hyprland
     flake.modules.nixos.programs-iotop
     flake.modules.nixos.programs-packages-common
     flake.modules.nixos.programs-sudo
     flake.modules.nixos.programs-terminfo
     flake.modules.nixos.prometheus-exporters-node
     flake.modules.nixos.prometheus-exporters-zfs
+    flake.modules.nixos.services-adb
     flake.modules.nixos.services-deploy-rs
     flake.modules.nixos.services-fail2ban
     flake.modules.nixos.services-openssh
     flake.modules.nixos.services-pipewire
+    flake.modules.nixos.services-removable-devices
     flake.modules.nixos.services-tailscale
     flake.modules.nixos.system-common
+    flake.modules.nixos.system-fonts
     flake.modules.nixos.users-delay
   ];
 
   # System config.
   node = {
-    boot.initrd.ssh-unlock.kernelModules = ["e1000e"];
-
     fs.zfs = {
       hostId = "be2d9ac1";
       system = {
         disk = "/dev/disk/by-id/nvme-Samsung_SSD_950_PRO_512GB_S2GMNCAGB32083T";
         luksPasswordFile = "/tmp/root-disk-encryption.key";
-        swapSize = "16G";
+        swapSize = "20G"; # Size of RAM + square root of RAM for hibernate
       };
+    };
+
+    networking.bluetooth = {
+      powerOnBoot = true;
+      enableA2DPSink = true;
+      enableFastConnectable = true;
     };
 
     users.delay.ssh.authorizeTailscaleInternalKey = true;
@@ -73,6 +80,7 @@
 
   networking = {
     inherit hostName;
+    domain = "qyrnl.com";
     interfaces = {
       eno1.useDHCP = true;
       wlp3s0.useDHCP = true;
--- hosts/skl/users/delay.nix
+++ hosts/skl/users/delay.nix
@@ -1,19 +1,58 @@
 {
+  # Blueprint parameters.
   flake,
   inputs,
+  perSystem,
+  # Module parameters.
+  lib,
+  config,
   ...
 }: {
   imports = [
     inputs.nix-config-secrets.modules.home.blueprint
     inputs.nix-config-secrets.modules.home.services-atuin
+    inputs.nix-config-secrets.modules.home.services-cachix
+    inputs.nix-config-secrets.modules.home.ssh-keys-ring-0-tier
 
     flake.modules.home.account-essentials
+    flake.modules.home.browsers
+    flake.modules.home.cachix
+    flake.modules.home.desktop-essentials
+    flake.modules.home.fonts
+    flake.modules.home.ghostty
     flake.modules.home.home-manager-nixos
     flake.modules.home.jujutsu-deprecated
+    flake.modules.home.keychain
+    flake.modules.home.pkgs-desktop-gui
+    flake.modules.home.pkgs-desktop-tui
     flake.modules.home.secrets
+    flake.modules.home.ssh-forgejo
+    flake.modules.home.ssh-keys
+    flake.modules.home.usb-auto-mount
+    flake.modules.home.walker
+    flake.modules.home.wayland-essentials
+    flake.modules.home.wayland-hyprland
+    flake.modules.home.wayland-notifications
+    flake.modules.home.wayland-quickshell
   ];
 
   home.stateVersion = "25.05";
 
-  node.services.atuin.enableSync = true;
+  node = {
+    openssh.trusted-tier.ring = 0;
+    services.atuin.enableSync = true;
+    wayland = {
+      hyprland.monitor = "DP-1, 3840x2160@59.997Hz, 0x0, 1.25";
+
+      display.logicalResolution = {
+        width = 3072;
+        height = 1728;
+      };
+
+      uwsm-wrapper = {
+        package = perSystem.self.app2unit;
+        prefix = "${lib.getExe config.node.wayland.uwsm-wrapper.package} --";
+      };
+    };
+  };
 }
