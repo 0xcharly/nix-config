0a71e23c8231c65c07ffe7c455a2e816847beec5
Charly Delay <charly@delay.gg>
2026-03-15 13:40:04 +0900

    Finetune Ghostty config

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -34,27 +34,45 @@ in
             font_name
           ]
         );
-        window-padding-x = 4;
-        window-padding-y = 4;
+        window-padding-x = 2;
+        window-padding-y = 2;
         window-padding-balance = true;
+        window-padding-color = "extend";
+        unfocused-split-opacity = 1;
         shell-integration-features = "no-cursor,no-title";
+        notify-on-command-finish = "unfocused";
+        notify-on-command-finish-action = "no-bell,notify";
         confirm-close-surface = false;
         auto-update = "off";
         custom-shader = "${./ghostty-cursor-trail.glsl}";
         adjust-cursor-thickness = 2;
         keybind = [
           "super+shift+comma=reload_config"
-          "shift+insert=paste_from_selection"
           "ctrl+tab=next_tab"
           "ctrl+shift+tab=previous_tab"
+
+          "ctrl+shift+h=goto_split:left"
+          "ctrl+shift+j=goto_split:bottom"
+          "ctrl+shift+k=goto_split:top"
+          "ctrl+shift+l=goto_split:right"
+
+          "ctrl+a>h=new_split:left"
+          "ctrl+a>j=new_split:down"
+          "ctrl+a>k=new_split:up"
+          "ctrl+a>l=new_split:right"
+          "ctrl+a>f=toggle_split_zoom"
+
+          "ctrl+a>n=next_tab"
+          "ctrl+a>p=previous_tab"
         ]
         ++ (
           let
             mod = if isDarwin then "super" else "ctrl";
           in
           [
-            "${mod}+shift+n=new_window" # Prevents conflicts with harpoon.
-            "${mod}+shift+t=new_tab" # Prevents conflicts with harpoon.
+            "${mod}+n=new_window"
+            "${mod}+t=new_tab"
+            "${mod}+shift+t=toggle_tab_overview"
             "${mod}+shift+w=close_surface"
             "${mod}+shift+c=copy_to_clipboard"
             "${mod}+shift+v=paste_from_clipboard"
@@ -72,8 +90,7 @@ in
         ];
       }
       // lib.optionalAttrs isLinux {
-        gtk-single-instance = true;
-        gtk-titlebar = false;
+        window-show-tab-bar = "never";
       }
       // lib.optionalAttrs isDarwin {
         macos-titlebar-proxy-icon = "hidden";
