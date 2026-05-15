2b0dbf756e8187470246084db17c9c350aa310d8
Charly Delay <charly@delay.gg>
2026-05-16 00:49:13 +0900

    Update caddy's SHA

--- modules/nixos/selfhosted-reverse-proxy-qyrnl-dot-com.nix
+++ modules/nixos/selfhosted-reverse-proxy-qyrnl-dot-com.nix
@@ -37,7 +37,7 @@
           inherit (cfg) enable;
           package = pkgs.caddy.withPlugins {
             plugins = [ "github.com/caddy-dns/gandi@v1.1.0" ];
-            hash = "sha256-XT6v5oOgHe/OHzhXgX3MhPyugyva8oEPd49Z+HAO5yY=";
+            hash = "sha256-p2ez5zWRY0NBlMwFrkCVylurYCh/RTxj0dvdNYpXsGI=";
           };
           environmentFile = config.age.secrets."services/gandi-creds.qyrnl.com".path;
           virtualHosts = {
