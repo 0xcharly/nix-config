f76526e99e9d284af6fd4357eaf7bb05a0a2ac38
Charly Delay <charly@delay.gg>
2026-05-18 17:19:00 +0900

    skl: rotate screen

--- hosts/skl/users/delay.nix
+++ hosts/skl/users/delay.nix
@@ -10,7 +10,20 @@
 
   node = {
     wayland = {
-      hyprland.monitor = "DP-1, 3840x2160@59.997Hz, 0x0, 1.25";
+      # https://wiki.hypr.land/0.41.2/Configuring/Monitors/#rotating
+      #
+      # | Transform              | x |
+      # + ---------------------- + - +
+      # | normal (no transforms) | 0 |
+      # | 90 degrees             | 1 |
+      # | 180 degrees            | 2 |
+      # | 270 degrees            | 3 |
+      # | flipped                | 4 |
+      # | flipped + 90 degrees   | 5 |
+      # | flipped + 180 degrees  | 6 |
+      # | flipped + 270 degrees  | 7 |
+      #
+      hyprland.monitor = "DP-1, 3840x2160@59.997Hz, 0x0, 1.25, transform, 1";
 
       display.logicalResolution = {
         width = 3072;
