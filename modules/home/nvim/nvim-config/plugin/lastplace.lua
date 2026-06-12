096bdedfb11e359db6d91112f6f076c496938b6f
Charly Delay <charly@delay.gg>
2026-06-13 00:09:44 +0900

    fmt: add stylua

--- modules/home/nvim/nvim-config/plugin/lastplace.lua
+++ modules/home/nvim/nvim-config/plugin/lastplace.lua
@@ -1,3 +1,3 @@
-vim.opt.viewoptions = "cursor" -- what gets saved in the session
-vim.api.nvim_create_autocmd("BufWinLeave", { command = "silent! mkview" })
-vim.api.nvim_create_autocmd("BufWinEnter", { command = "silent! loadview" })
+vim.opt.viewoptions = 'cursor' -- what gets saved in the session
+vim.api.nvim_create_autocmd('BufWinLeave', { command = 'silent! mkview' })
+vim.api.nvim_create_autocmd('BufWinEnter', { command = 'silent! loadview' })
