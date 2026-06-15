c9fe0c47fb0b4282201b13f53df5786478782438
Charly Delay <charly@delay.gg>
2026-06-15 14:20:02 +0900

    Rename node-x1p to term-x1p

--- modules/nixos.hosts/term-x1p/term-x1p.nix
+++ modules/nixos.hosts/term-x1p/term-x1p.nix
@@ -1,6 +1,6 @@
 { self, inputs, ... }:
 {
-  my.hosts.node-x1p = {
+  my.hosts.term-x1p = {
     stateVersion = "25.11";
 
     nixosModule =
@@ -99,19 +99,6 @@
 
       node = {
         wayland = {
-          # https://wiki.hypr.land/0.41.2/Configuring/Monitors/#rotating
-          #
-          # | Transform              | x |
-          # + ---------------------- + - +
-          # | normal (no transforms) | 0 |
-          # | 90 degrees             | 1 |
-          # | 180 degrees            | 2 |
-          # | 270 degrees            | 3 |
-          # | flipped                | 4 |
-          # | flipped + 90 degrees   | 5 |
-          # | flipped + 180 degrees  | 6 |
-          # | flipped + 270 degrees  | 7 |
-          #
           hyprland.monitors = [
             {
               output = "HDMA-A-1";
