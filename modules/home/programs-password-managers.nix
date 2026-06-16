f850c839fe0968fe2abb61f71a7f4bbc56433746
Charly Delay <charly@delay.gg>
2026-06-16 22:37:13 +0900

    Uninstall Electron-based Proton Pass app

--- modules/home/programs-password-managers.nix
+++ modules/home/programs-password-managers.nix
@@ -3,14 +3,15 @@
   flake.homeModules.programs-password-managers = {
     imports = with self.homeModules; [
       programs-1password
-      # # TODO(26.11): bitwarden-desktop package is broken
+      # NOTE: do not install these app wrappers as they depend on electron
+      # NOTE: bitwarden-desktop package is broken in 26.05
       # programs-bitwarden
-      programs-proton-pass
+      # programs-proton-pass
     ];
 
     xdg.mimeApps = {
-      associations.added."x-scheme-handler/otpauth" = "proton-pass.desktop";
-      defaultApplications."x-scheme-handler/otpauth" = "proton-pass.desktop";
+      associations.added."x-scheme-handler/otpauth" = "1password.desktop";
+      defaultApplications."x-scheme-handler/otpauth" = "1password.desktop";
     };
   };
 }
