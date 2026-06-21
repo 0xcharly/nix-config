072dad133b137298acc7816c82a200582466edb6
Charly Delay <charly@delay.gg>
2026-06-21 16:51:43 +0900

    term-x1p: update monitor orientation

--- modules/nixos.hosts/term-x1p/term-x1p.nix
+++ modules/nixos.hosts/term-x1p/term-x1p.nix
@@ -98,7 +98,7 @@
               output = "HDMI-A-1";
               mode = "3840x2160@60";
               scale = 1.5;
-              transform = 1;
+              transform = 3;
             }
           ];
 
