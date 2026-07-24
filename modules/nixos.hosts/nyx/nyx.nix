d892adfbbdd20b61953431e3c20683656bcb21dc
Charly Delay <charly@delay.gg>
2026-07-24 15:19:46 +0900

    nyx: enable initrd hoopsnake for remote LUKS unlock
    Cold-booted or hibernated nyx stalls invisibly at the LUKS prompt;
    initrd-hoopsnake registers nyx-unlock on the tailnet so the prompt is
    reachable over SSH.
    
    Secret paths are overridden because the hoopsnake secrets are
    registered under the tailnet machine name (term-nyx), not
    networking.hostName (nyx) that the module defaults derive. NIC driver
    is igc (Intel I226-V).

--- modules/nixos.hosts/nyx/nyx.nix
+++ modules/nixos.hosts/nyx/nyx.nix
@@ -3,85 +3,98 @@
   my.hosts.nyx = {
     stateVersion = "25.05";
 
-    nixosModule = {
-      imports = [
-        inputs.nix-config-secrets.nixosModules.default
-        inputs.nix-config-secrets.nixosModules.jptax-fa5003-inputs
-        inputs.nix-config-secrets.nixosModules.luks-remote-unlock
-        inputs.nix-config-secrets.nixosModules.mail-account-delay
-        inputs.nix-config-secrets.nixosModules.services-hoopsnake-term-nyx
-        inputs.nix-config-secrets.nixosModules.services-tailscale
-        inputs.nix-config-secrets.nixosModules.ssh-keys-ring-0-tier
-        inputs.nix-config-secrets.nixosModules.users-delay
+    nixosModule =
+      { config, ... }:
+      {
+        imports = [
+          inputs.nix-config-secrets.nixosModules.default
+          inputs.nix-config-secrets.nixosModules.jptax-fa5003-inputs
+          inputs.nix-config-secrets.nixosModules.luks-remote-unlock
+          inputs.nix-config-secrets.nixosModules.mail-account-delay
+          inputs.nix-config-secrets.nixosModules.services-hoopsnake-term-nyx
+          inputs.nix-config-secrets.nixosModules.services-tailscale
+          inputs.nix-config-secrets.nixosModules.ssh-keys-ring-0-tier
+          inputs.nix-config-secrets.nixosModules.users-delay
 
-        self.nixosModules.profile-hardware-workstation
-        self.nixosModules.profile-ssh-identities-ring0
+          self.nixosModules.profile-hardware-workstation
+          self.nixosModules.profile-ssh-identities-ring0
 
-        self.nixosModules.bootloader-systemd-boot
-        self.nixosModules.fs-zfs-common
-        self.nixosModules.fs-zfs-system-base
-        self.nixosModules.fs-zfs-system
-        self.nixosModules.fs-zfs-zpool-root
-        self.nixosModules.fs-zfs-zpool-root-home
-        self.nixosModules.hardware-cpu-amd
-        self.nixosModules.hardware-gpu-amd
-        self.nixosModules.hardware-wake-on-lan
-        self.nixosModules.networking-common
-        self.nixosModules.networking-resolved
-        self.nixosModules.nix
-        self.nixosModules.nix-build-aarch64
-        self.nixosModules.nixpkgs
-        self.nixosModules.programs-apdbctl
-        self.nixosModules.programs-essentials
-        self.nixosModules.programs-gnome-calendar
-        self.nixosModules.programs-greetd
-        self.nixosModules.programs-greetd-autologin
-        self.nixosModules.programs-iotop
-        self.nixosModules.programs-packages-common
-        self.nixosModules.programs-secrets
-        self.nixosModules.programs-steam
-        self.nixosModules.programs-sudo
-        self.nixosModules.programs-terminfo
-        self.nixosModules.prometheus-exporters-node
-        self.nixosModules.prometheus-exporters-smartctl
-        self.nixosModules.prometheus-exporters-zfs
-        self.nixosModules.services-adb
-        self.nixosModules.services-fail2ban
-        self.nixosModules.services-openssh
-        self.nixosModules.services-pipewire
-        self.nixosModules.services-removable-devices
-        self.nixosModules.services-tailscale
-        self.nixosModules.services-zmk-studio
-        self.nixosModules.system-common
-        self.nixosModules.system-fonts
-        self.nixosModules.users-delay
-      ];
+          self.nixosModules.bootloader-systemd-boot
+          self.nixosModules.fs-zfs-common
+          self.nixosModules.fs-zfs-system-base
+          self.nixosModules.fs-zfs-system
+          self.nixosModules.fs-zfs-zpool-root
+          self.nixosModules.fs-zfs-zpool-root-home
+          self.nixosModules.hardware-cpu-amd
+          self.nixosModules.hardware-gpu-amd
+          self.nixosModules.hardware-wake-on-lan
+          self.nixosModules.initrd-hoopsnake
+          self.nixosModules.networking-common
+          self.nixosModules.networking-resolved
+          self.nixosModules.nix
+          self.nixosModules.nix-build-aarch64
+          self.nixosModules.nixpkgs
+          self.nixosModules.programs-apdbctl
+          self.nixosModules.programs-essentials
+          self.nixosModules.programs-gnome-calendar
+          self.nixosModules.programs-greetd
+          self.nixosModules.programs-greetd-autologin
+          self.nixosModules.programs-iotop
+          self.nixosModules.programs-packages-common
+          self.nixosModules.programs-secrets
+          self.nixosModules.programs-steam
+          self.nixosModules.programs-sudo
+          self.nixosModules.programs-terminfo
+          self.nixosModules.prometheus-exporters-node
+          self.nixosModules.prometheus-exporters-smartctl
+          self.nixosModules.prometheus-exporters-zfs
+          self.nixosModules.services-adb
+          self.nixosModules.services-fail2ban
+          self.nixosModules.services-openssh
+          self.nixosModules.services-pipewire
+          self.nixosModules.services-removable-devices
+          self.nixosModules.services-tailscale
+          self.nixosModules.services-zmk-studio
+          self.nixosModules.system-common
+          self.nixosModules.system-fonts
+          self.nixosModules.users-delay
+        ];
 
-      # System config
-      node = {
-        fs.zfs = {
-          hostId = "0a52fab4";
-          system = {
-            disk = "/dev/disk/by-id/nvme-CT4000T700SSD3_2340E87BB2E0";
-            luksPasswordFile = "/tmp/root-disk-encryption.key";
-            swapSize = "72G"; # Size of RAM + square root of RAM for hibernate
+        # System config
+        node = {
+          boot.initrd.hoopsnake = {
+            kernelModules = [ "igc" ];
+
+            # Secrets are registered under the tailnet machine name (term-nyx),
+            # not networking.hostName (nyx) that the module defaults derive.
+            clientIdFile = config.age.secrets."services/hoopsnake/term-nyx/tailscale-client-id".path;
+            clientSecretFile = config.age.secrets."services/hoopsnake/term-nyx/tailscale-client-secret".path;
+            privateHostKeyFile = config.age.secrets."services/hoopsnake/term-nyx/ssh_host_ed25519_key".path;
           };
+
+          fs.zfs = {
+            hostId = "0a52fab4";
+            system = {
+              disk = "/dev/disk/by-id/nvme-CT4000T700SSD3_2340E87BB2E0";
+              luksPasswordFile = "/tmp/root-disk-encryption.key";
+              swapSize = "72G"; # Size of RAM + square root of RAM for hibernate
+            };
+          };
+
+          networking.wakeOnLan.interface = "enp115s0";
+          users.delay.ssh.authorizeTailscaleInternalKey = true;
         };
 
-        networking.wakeOnLan.interface = "enp115s0";
-        users.delay.ssh.authorizeTailscaleInternalKey = true;
+        boot.initrd.availableKernelModules = [
+          "ahci"
+          "nvme"
+          "sd_mod"
+          "usbhid"
+          "xhci_pci"
+        ];
+
+        networking.interfaces.enp115s0.useDHCP = true;
       };
-
-      boot.initrd.availableKernelModules = [
-        "ahci"
-        "nvme"
-        "sd_mod"
-        "usbhid"
-        "xhci_pci"
-      ];
-
-      networking.interfaces.enp115s0.useDHCP = true;
-    };
 
     users.delay.imports = with self.homeModules; [
       profile-hardware-workstation
